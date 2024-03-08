a9728a884dce21108d06eef6a9e5cc63836018cd
Ruchika Shakya <139382679+ruchikadeveloper@users.noreply.github.com>
2024-03-09 01:08:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// function changeBackground(imageUrl) {
+//     var backgroundImageContainer = document.getElementById('backgroundImageContainer');
+//     backgroundImageContainer.style.backgroundImage = 'url(' + imageUrl + ')';
+//     backgroundImageContainer.style.backgroundSize = 'cover';
+//     backgroundImageContainer.style.backgroundPosition = 'center';
+//     backgroundImageContainer.style.height = '300px'; // Adjust the height as needed
+// }
